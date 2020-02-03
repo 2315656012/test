cfba52a37a0bbb15992ad3ea78d9987c14edae0f
rainbow <2315656012@qq.com>
2020-02-03 23:34:52 +0800

    新建了启动类

--- src/main/java/com/rainbow/test/APPMain01.java
+++ src/main/java/com/rainbow/test/APPMain01.java
@@ -4,7 +4,7 @@ public class APPMain01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hellw world");
+		System.out.println("hellw world");
 	}
 
 }
