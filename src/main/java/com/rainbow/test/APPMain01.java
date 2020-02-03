1dd01469cbcbd2cbc4fa6745f354585dd428f202
rainbow <2315656012@qq.com>
2020-02-03 22:53:15 +0800

    首次提交

---
+++ src/main/java/com/rainbow/test/APPMain01.java
@@ -0,0 +1,10 @@
+package com.rainbow.test;
+
+public class APPMain01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hellw world");
+	}
+
+}
